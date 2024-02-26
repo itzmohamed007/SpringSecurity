a7848c294b307dc99c17a1840b54640b30eab8b1
= <itzmohamed007@gmail.com>
2024-02-26 01:14:21 +0100

    updated user details service bean to throw UsernameNotFoundException instead of RuntimeException

--- security/src/main/java/com/security/security/config/appConfig.java
+++ security/src/main/java/com/security/security/config/appConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -23,7 +24,7 @@ public class appConfig {
     @Bean
     public UserDetailsService userDetailsService() {
         return username -> repository.findUserByEmail(username).orElseThrow(
-                () -> new RuntimeException("User not found")
+                () -> new UsernameNotFoundException("User not found")
         );
     }
 
